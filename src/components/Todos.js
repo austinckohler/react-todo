77f25191d46c7bab6ec93e30287a8fd485087ac5
austinckohler <austinckohler@gmail.com>
2020-06-04 21:53:31 -0600

    more destructuring

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -3,15 +3,30 @@ import TodoItem from "./TodoItem";
 
 class Todos extends Component {
   render() {
+    const { markComplete, delTodo } = this.props;
     return this.props.todos.map((todo) => (
       <TodoItem
         key={todo.id}
         todo={todo}
-        markComplete={this.props.markComplete}
-        delTodo={this.props.delTodo}
+        markComplete={markComplete}
+        delTodo={delTodo}
       />
     ));
   }
 }
 
 export default Todos;
+
+//   showTodos = () =>
+//     this.props.todos.map((todo) => (
+//       <TodoItem
+//         key={todo.id}
+//         todo={todo}
+//         markComplete={this.props.markComplete}
+//         delTodo={this.props.delTodo}
+//       />
+//     ));
+//   render() {
+//     this.showTodos;
+//   }
+// }
